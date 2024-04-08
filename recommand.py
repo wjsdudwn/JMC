0a81a4d5f2e19fd7efa6536d3215182c405510c8
wjsdudwn <juyj0704@gmail.com>
2024-04-08 22:00:34 +0900

    ,

---
+++ recommand.py
@@ -0,0 +1,22 @@
+# menu type, talking, sex, 
+
+class Profile:
+    def __init__(self):
+        self.menu = ''
+        self.sex = ''
+        self.talking = ''
+        self.age = ''
+    
+    def settingProps(self):
+        # prototype
+        self.menu = input("input menu")
+        self.sex = input("input sex")
+        self.talking = input("input personality")
+        self.age = input("input age")
+    
+
+
+
+
+
+    
