ef3d8de8bc905e8ebd0b1bd59963773b5ecb1d53
wjsdudwn <juyj0704@gmail.com>
2024-04-08 23:00:51 +0900

    ,.

---
+++ UserProfile.py
@@ -0,0 +1,14 @@
+# menu type, talking, sex, 
+
+class UserProfile:
+    def __init__(self):
+        self.menu = ''
+        self.sex = ''
+        self.talking = ''
+        self.age = ''
+
+
+
+
+
+    
---
+++ main.py
@@ -0,0 +1,12 @@
+from UserProfile import UserProfile
+
+# compare two user (temporary)
+
+user1 = UserProfile()
+user2 = UserProfile()
+
+user1.sex = "male"
+
+print(user1.sex)
+
+
